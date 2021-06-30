bcfd7ceba78d93f0c5293597f47fc581804be99d
ssook <chlthr1222@naver.com>
2021-06-30 19:43:49 +0900

    CREATE : 시작 화면
    시작 화면 전체적으로 추가

--- app/src/main/java/com/kotlin/speedsea/MainActivity.kt
+++ app/src/main/java/com/kotlin/speedsea/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.kotlin.speedsea
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var start: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, secondActivity::class.java)
+
+        start = findViewById(R.id.start) as Button
+        start.setOnClickListener {
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/kotlin/speedsea/secondActivity.kt
@@ -0,0 +1,30 @@
+package com.kotlin.speedsea
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class secondActivity : AppCompatActivity() {
+
+    private lateinit var battle: Button
+    private lateinit var story: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        story = findViewById(R.id.story) as Button
+        story.setOnClickListener {
+            var intent = Intent(this, thirdActivity::class.java)
+            startActivity(intent)
+        }
+
+        battle = findViewById(R.id.battle) as Button
+        battle.setOnClickListener {
+            var toastMsg = Toast.makeText(this, "추후 업데이트 할 예정입니다.", Toast.LENGTH_SHORT)
+            toastMsg.show()
+        }
+    }
+}
