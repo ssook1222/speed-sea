3cc9bac7b80b3865cb0ceff1c7e279155bf39e85
ssook <chlthr1222@naver.com>
2021-07-01 15:24:01 +0900

    UPDATE : 타이머 기능 구현
    - 60초가 지나면 초기화
    - 핸들러 사용하여 쓰레드에서 액티비티에 토스트 창 띄우기

--- app/src/main/java/com/kotlin/speedsea/thirteenActivity.kt
+++ app/src/main/java/com/kotlin/speedsea/thirteenActivity.kt
@@ -2,18 +2,24 @@
 package com.kotlin.speedsea
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.kotlin.speedsea.thirteenActivity
 import java.util.*
 import kotlin.concurrent.timer
 
+
 class thirteenActivity : AppCompatActivity() {
-    private  var time = 60
-    private var timerTask : Timer? = null
+    private var time = 60
+    private var timerTask: Timer? = null
+    private var sec: Int? = null
 
     private lateinit var one: Button
     private lateinit var two: Button
@@ -21,14 +27,17 @@ class thirteenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_thirteen)
         startTimer()
 
         one = findViewById(R.id.one) as Button
         one.setOnClickListener {
             var intent = Intent(this, fourteenActivity::class.java)
-            intent.putExtra("timer",time)
+            intent.putExtra("timer", sec)
             startActivity(intent)
         }
         two = findViewById(R.id.two) as Button
@@ -42,13 +51,28 @@ class thirteenActivity : AppCompatActivity() {
             toastMsg.show()
         }
     }
-    private fun startTimer(){
-        timerTask=timer(period=10){
+
+    private fun startTimer() {
+        timerTask = timer(period = 10){
             time--;
-            val timer=findViewById(R.id.timer) as TextView
-            val sec=time/100
-            runOnUiThread{
+            val timer = findViewById(R.id.timer) as TextView
+            sec = 60 + (time / 100)
+            runOnUiThread {
                 timer.text = "${sec}"
+            }
+            Log.d("남은 시간","띠용"+sec)
+            if(sec==0){
+                timerTask?.cancel();
+                val handler = Handler(Looper.getMainLooper())
+                handler.postDelayed(Runnable {
+                    Toast.makeText(
+                        this@thirteenActivity,
+                        "실패!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }, 0)
+                var retry = Intent(this@thirteenActivity, thirteenActivity::class.java)
+                startActivity(retry)
             }
         }
     }
